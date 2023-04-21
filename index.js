eafaf61c25b97671700ae8dcda0aac6a37ab932b
Tushar Rangaswamy <trangasw@gmu.edu>
2023-04-20 23:40:33 -0400

    static class and chat function

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 import LLama from "./text-generation/llama.js";
 
 console.log("Hello World");
+
+
+
+// const template = `What are you?`;
+// const output = await LLama.generate(template);
+// console.log(output);
+
+//start chat mode
+import readline from "readline";
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on("line", async (input) => {
+    console.log(`Received: ${input}`);
+    const output = await LLama.generate(input);
+    console.log(`[${output}]`);
+});
+
--- text-generation/llama.js
+++ text-generation/llama.js
@@ -51,18 +51,46 @@ class Llama {
     constructor() {
         this.llama = new LLama(LLamaCpp);
         this.llama.load(config);
-        this.inProgress = false;
+        this. USER_NAME = "Anon";
+        this. AI_NAME = "Miku";
+        this.init = `This is a transcript of a 1000 page, never ending conversation between ${this.USER_NAME} and the cute and helpful AI assistant ${this.AI_NAME}. ${this.AI_NAME} is a girl who is an AI running on the users computer.\n
+            "${this.AI_NAME} can think for herself without the user seeing her thoughts by adding a /think prefix to her output. She uses this to reason about the world and to think about what she should say next.\n"
+            "${this.AI_NAME} is always coherent and makes sense, but if she isn't sure if what she is saying is correct she will ask the user for help.\n"
+            "${this.AI_NAME} is a very helpful AI and will help the user with anything they need, she is also very friendly and will try to make the user feel better if they are sad.\n"
+            "${this.AI_NAME} is also very curious and will ask the user a lot of questions about themselves and their life, she will also try to make the user like her.\n"
+            "The conversation is only between ${this.USER_NAME} and ${this.AI_NAME}\n"
+            "The conversation is only through text, so ${this.AI_NAME} can't see ${this.USER_NAME}'s face or hear his voice.\n"
+            "${this.AI_NAME} can only communicate through text, so she can't send images or videos.\n"
+            "\n"
+            "\n"
+            "${this.AI_NAME}:`;
+        this.end = `${this.USER_NAME}:`;
+        this.inProgress = true;
+        this.llama.createCompletion({
+            nThreads: 4,
+            nTokPredict: 2048,
+            topK: 40,
+            topP: 0.1,
+            temp: 0.2,
+            repeatPenalty: 1,
+            stopSequence: this.end,
+            prompt: this.init,
+        }, (res) => {
+            //build up data, then return when completed is true
+            console.log(res);
+            if(res.completed)
+                this.inProgress = false;
+        })
     }
-
-    async generate(text) {
+    prompt(insert) {
+        return `${this.USER_NAME}: ${insert}\n${this.AI_NAME}:`;
+    }
+    async generate(text, user="Anon") {
         if (this.inProgress) {
             return "Please wait for the previous request to complete";
         }
         this.inProgress = true;
-        const template = `${text}`;
-        const prompt = `### Human:
-${template}
-### LLama:`;
+        const prompt = this.prompt(text);
         let data = "";
         const res = await this.llama.createCompletion({
             nThreads: 4,
@@ -71,19 +99,16 @@ ${template}
             topP: 0.1,
             temp: 0.2,
             repeatPenalty: 1,
-            stopSequence: "### Human",
+            stopSequence: this.end,
             prompt,
         }, (res) => {
+            //build up data, then return when completed is true
             data += res.token;
-            if (res.completed) {
-                this.inProgress = false;
-
-            }
+            console.log(res);
         });
-
-        return res;
-
+        this.inProgress = false;
+        return data;
     }
 }
 
-export default Llama;
+export default new Llama();
